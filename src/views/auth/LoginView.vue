cb9bc0470f1de38c0ecb059bbcaddbceb690ea9a
kanggyeonggu <rudrn0110@naver.com>
2025-05-14 01:58:38 +0900

    feat: 에러 메시지 출력 컴포넌트 추가

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,5 +1,13 @@
 <template>
+
   <div class="min-h-[calc(100vh-48px)] flex items-center justify-center bg-white">
+        <!-- Notification 컴포넌트 -->
+    <Notification
+      v-if="message"
+      :message="message"
+      :key="messageKey"
+      @clear-message="clearMessage"
+    />
     <div class="transform -translate-y-10 w-full max-w-lg p-6">
       <h2 class="text-2xl font-semibold text-center text-gray-900 mb-6 font-apple-sdgothic">
         Wanna enjoy your trip?
@@ -7,22 +15,36 @@
 
       <!-- 아이디 입력 -->
       <div class="relative mb-0">
-        <input v-model="username" type="text" id="username" ref="usernameInput" @focus="isUsernameFocused = true"
-          @blur="isUsernameFocused = false" @keyup.enter="showPasswordField" :class="[
+        <input
+          v-model="username"
+          type="text"
+          id="username"
+          ref="usernameInput"
+          @focus="isUsernameFocused = true"
+          @blur="isUsernameFocused = false"
+          @keyup.enter="showPasswordField"
+          :class="[
             'peer w-full max-w-lg pt-6 pb-2 px-4 border border-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500',
             showPassword ? 'rounded-t-xl rounded-b-none' : 'rounded-xl'
-          ]" autocomplete="username" />
-        <label :class="[
-          'absolute left-4 text-gray-500 transition-all duration-200 pointer-events-none',
-          username || isUsernameFocused
-            ? 'text-sm top-1 text-blue-800'
-            : 'text-base top-3.5'
-        ]">
+          ]"
+          autocomplete="username"
+        />
+        <label
+          :class="[
+            'absolute left-4 text-gray-500 transition-all duration-200 pointer-events-none',
+            username || isUsernameFocused
+              ? 'text-sm top-1 text-blue-800'
+              : 'text-base top-3.5'
+          ]"
+        >
           아이디
         </label>
 
-        <button v-if="!showPassword" @click="showPasswordField"
-          class="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-blue-500">
+        <button
+          v-if="!showPassword"
+          @click="showPasswordField"
+          class="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-blue-500"
+        >
           <img src="@/assets/icons/send_right_arrow.png" alt="다음" class="w-5 h-5" />
         </button>
       </div>
@@ -30,35 +52,35 @@
       <!-- 비밀번호 입력 -->
       <transition name="fade">
         <div v-if="showPassword" class="relative mt-0">
-          <input v-model="password" type="password" id="password" ref="passwordInput" @focus="isPasswordFocused = true"
-            @blur="isPasswordFocused = false" @keyup.enter="login"
+          <input
+            v-model="password"
+            type="password"
+            id="password"
+            ref="passwordInput"
+            @focus="isPasswordFocused = true"
+            @blur="isPasswordFocused = false"
+            @keyup.enter="login"
             class="peer w-full pt-6 pb-2 px-4 border border-gray-500 border-t-0 rounded-b-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
-            autocomplete="current-password" />
-          <label :class="[
-            'absolute left-4 text-gray-500 transition-all duration-200 pointer-events-none',
-            password || isPasswordFocused
-              ? 'text-sm top-1 text-blue-800'
-              : 'text-base top-3.5'
-          ]">
+            autocomplete="current-password"
+          />
+          <label
+            :class="[
+              'absolute left-4 text-gray-500 transition-all duration-200 pointer-events-none',
+              password || isPasswordFocused
+                ? 'text-sm top-1 text-blue-800'
+                : 'text-base top-3.5'
+            ]"
+          >
             비밀번호
           </label>
-          <button 
-            class="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-blue-500"  
+          <button
+            class="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-blue-500"
             @click="login"
           >
-            <img
-              src="@/assets/icons/send_right_arrow.png"
-              alt="로그인" 
-              class="w-5 h-5"
-            >
+            <img src="@/assets/icons/send_right_arrow.png" alt="로그인" class="w-5 h-5" />
           </button>
         </div>
       </transition>
-
-      <!-- 로그인 실패 메시지 -->
-      <p v-if="message" class="text-center text-red-500 mt-4">
-        {{ message }}
-      </p>
 
       <!-- 추가 기능 영역 -->
       <div class="mt-32 text-center space-y-3 text-sm text-gray-600">
@@ -71,7 +93,7 @@
 
         <!-- 암호를 잊으셨습니까 -->
         <div>
-          <router-link 
+          <router-link
             to="/forgot-password"
             class="mt-6 text-xs inline-flex items-center justify-center text-blue-500 hover:underline ml-1"
           >
@@ -83,8 +105,8 @@
         <!-- SSAFY 계정 생성 -->
         <div class="text-xs">
           SSAFY 계정이 없습니까?
-          <router-link 
-            to="/signup" 
+          <router-link
+            to="/signup"
             class="text-xs inline-flex items-center text-blue-500 hover:underline ml-1"
           >
             <span>SSAFY 계정 생성</span>
@@ -98,6 +120,7 @@
 
 <script setup>
 import { ArrowUpRight } from 'lucide-vue-next';
+import Notification from '@/components/alert/Notification.vue';
 </script>
 
 <script>
@@ -105,15 +128,19 @@ import axios from 'axios';
 import TokenService from '../../services/token.service';
 
 export default {
+  components: {
+    Notification,
+  },
   data() {
     return {
       username: '',
       password: '',
-      message: '',
+      message: '', // 로그인 실패 메시지
+      messageKey: 0, // Notification 컴포넌트의 고유 키
       showPassword: false,
       isUsernameFocused: false,
       isPasswordFocused: false,
-      rememberMe: false, // 계정 저장 체크박스
+      rememberMe: false,
     };
   },
   methods: {
@@ -144,13 +171,32 @@ export default {
           this.$store.commit('login', accessToken);
           this.$router.push({ name: 'home' });
         } else {
-          this.message = '로그인에 실패했습니다. 토큰이 없습니다.';
+          this.setMessage('로그인에 실패했습니다. 토큰이 없습니다.');
         }
       } catch (err) {
         console.error("로그인 중 오류 발생:", err);
-        this.message = '로그인 실패';
+        const errorMessage = '로그인에 실패했습니다. 다시 시도하세요.';
+        this.setMessage(errorMessage);
       }
+    },
+    setMessage(message) {
+      this.message = message;
+      this.messageKey += 1; // 메시지 설정 시 고유 키 증가
+    },
+    clearMessage() {
+      this.message = '';
     },
   },
 };
 </script>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
