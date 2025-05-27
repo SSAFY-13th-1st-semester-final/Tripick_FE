d1186ec6eb37ba0349223f7ba16b58ffc6da0d4f
rudrn0110 <rudrn0110@naver.com>
2025-05-27 15:20:28 +0900

    fix: 웰컴페이지 이미지 파일 경로 수정

--- src/assets/data/homeFeatures.js
+++ src/assets/data/homeFeatures.js
@@ -2,19 +2,19 @@
 export const homeFeatures = [
   {
     id: 1,
-    iconImage: "/src/assets/data/images/ai-icon.png",
+    iconImage: "@/assets/data/images/ai-icon.png",
     title: "스마트 AI 여행 일정 솔루션",
     description: "나의 여행 계획을 AI와 함께 스마트하게 관리하세요.",
   },
   {
     id: 2,
-    iconImage: "/src/assets/data/images/history-icon.png",
+    iconImage: "@/assets/data/images/history-icon.png",
     title: "나만의 여행, 함께 나누는 기쁨",
     description: "찰나의 순간을 기록하고, 공유해보세요.",
   },
   {
     id: 3,
-    iconImage: "/src/assets/data/images/mobile-icon.png",
+    iconImage: "@/assets/data/images/mobile-icon.png",
     title: "모바일 최적화",
     description: "언제 어디서나 모바일로 여행 정보를 확인하고 예약하세요.",
   },
